c6aa55bf12d484e3c5b450016b8dbc35f1b545b8
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-05-28 03:30:26 +0530

    DashBoard injected with Dummy Project List and Notifications

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './component/layout/Navbar';
+import Dashboard from './component/dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar/>
+        <Switch>
+          <Route path ='/' component={Dashboard}/>
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/component/project/ProjectList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const ProjectList =()=>{
+    return(
+        <div className="project-list section">
+            <div className="card zz-depth-1 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title"> Mario Plan</span>
+                    <p> Posted by Mario</p>
+                    <p className="grey-text">Sep 3, 2am</p>
+                </div>
+
+            </div>
+            <div className="card zz-depth-1 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title"> Mario Plan</span>
+                    <p> Posted by Mario</p>
+                    <p className="grey-text">Sep 3, 2am</p>
+                </div>
+
+            </div>
+            <div className="card zz-depth-1 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title"> Mario Plan</span>
+                    <p> Posted by Mario</p>
+                    <p className="grey-text">Sep 3, 2am</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList;
