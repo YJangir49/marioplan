3edb6745d869f14fd6604e36d2ba4723b46c6204
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-05-28 02:58:37 +0530

    Navbar with SignedIn and SignedOutLinks

---
+++ src/component/layout/SignedOutLinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const SignedOutLinks =()=>{
+       return(
+           <ul className="right">
+                <li> <NavLink to='/'> Sign Up</NavLink></li>
+                <li> <NavLink to='/'> Log In</NavLink></li>
+           </ul>
+       )
+}
+
+export default SignedOutLinks;
