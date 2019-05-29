a70e2e7959b7ca410b598993177ea54b2137d4cf
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-05-29 10:08:37 +0530

    Synced with Database

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../project/ProjectList';
 import {connect} from 'react-redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import {compose} from 'redux';
 
 class Dashboard extends Component {
     
@@ -22,9 +24,13 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps=(state)=>{
+    console.log(state);
     return{
-        projects: state.project.projects
+        projects: state.firestore.ordered.projects
     }
 }
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect(['projects'])
+    )(Dashboard);
--- src/index.js
+++ src/index.js
@@ -8,18 +8,20 @@ import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import {reduxFirestore,getFirestore} from 'redux-firestore';
-import {reactReduxFirebase,getFirebase} from 'react-redux-firebase';
-import fbConfig from './config/fbConfig'
+import { reactReduxFirebase,getFirebase} from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
 const store= createStore(rootReducer, 
     compose(
         applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
         reduxFirestore(fbConfig),
         reactReduxFirebase(fbConfig)
+
         )
     );
+    
 
-ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
